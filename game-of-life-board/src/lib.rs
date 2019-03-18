c2dd6898f775b62c5e31e08017a42c4647efba10
Pavel Savara <pavel.savara@ncr.com>
2019-03-18 23:51:10 +0100

    gol

--- game-of-life-board/src/lib.rs
+++ game-of-life-board/src/lib.rs
@@ -1,9 +1,12 @@
 extern crate wasm_bindgen;
 extern crate cfg_if;
+extern crate itertools;
 
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use itertools::Itertools;
+use std::{thread, time};
 
 #[wasm_bindgen]
 extern {
@@ -12,5 +15,123 @@ extern {
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, wasm!");
+    alert("Hello, !!wasm!");
+}
+
+#[derive(PartialEq, PartialOrd)]
+#[derive(Eq,Ord)]
+#[derive(Debug)]
+pub struct Position {
+    x:i32,
+    y:i32,
+}
+
+pub type Board = Vec<Position>;
+
+const OFFSETS_AROUND:[[i32;2];8] = [[-1,-1], [0,-1], [1,-1],
+                  [-1, 0],         [1, 0],
+                  [-1, 1], [0, 1], [1, 1]];
+
+fn rules(count: usize, is_alive: bool) -> bool{
+	count == 3 || (count == 2 && is_alive)
+}
+    
+pub fn gol(generation: &Board) -> Board {
+	generation.iter()
+	.map(|current:&Position| OFFSETS_AROUND.iter()
+		.map(|o| Position {
+				x: (current.x + o[0]),
+				y: (current.y + o[1])
+			}
+		).collect::<Board>()
+	)
+	.flatten()
+	.sorted()
+	.dedup()
+	.filter(|current:&Position| 
+		rules(OFFSETS_AROUND.iter()
+				.map(|o| Position {
+					x: (current.x + o[0]),
+					y: (current.y + o[1])})
+				.filter(|q| generation.contains(q))
+				.count(), 
+			generation.contains(current))
+	)
+	.collect::<Board>()
+}
+
+const WIDTH:usize = 30;
+const HEIGHT:usize = 30;
+
+pub fn render(generation: &Board) -> String {
+
+	let avg_x:i32 = generation.iter().map(|p| p.x).sum::<i32>()/(generation.len() as i32);
+	let avg_y:i32 = generation.iter().map(|p| p.y).sum::<i32>()/(generation.len() as i32);
+	let mut view:[[bool;WIDTH];HEIGHT] = [[false;WIDTH];HEIGHT];
+
+	for point in generation.iter() {
+        let shift_x:usize = (point.x - avg_x + (WIDTH / 2) as i32) as usize;
+        let shift_y:usize = (point.y - avg_y + (HEIGHT / 2)as i32)as usize;
+        if shift_x > 0 && shift_y > 0 && shift_x < WIDTH && shift_y < HEIGHT {
+            view[shift_y][shift_x] = true;
+        }
+	}
+
+	let mut sb=String::from("Hello, world!");
+
+	for y in 0..HEIGHT {
+		for x in 0..WIDTH {
+			if view[y][x]{
+				sb.push('X');
+			}
+			else{
+				sb.push(' ');
+			}
+		}
+		sb.push('\n');
+	}
+
+	sb
+}
+
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_blinker() {
+		let blinker:Board = vec![
+			Position {x:0,y:-1},
+			Position {x:0,y:0},
+			Position {x:0,y:1},
+		];
+		println!("{:?}", blinker);
+
+		let g2 = gol(&blinker);
+		println!("{:?}", g2);
+    }
+
+    #[test]
+    fn test_pentomimo() {
+		let millis = time::Duration::from_millis(100);
+		let pentomimo:Board = vec![
+			Position {x:-1,y:0},
+			Position {x:-1,y:1},
+			Position {x:0,y:-1},
+			Position {x:0,y:0},
+			Position {x:1,y:0},
+		];
+		println!("{:?}", pentomimo);
+
+		let mut board = pentomimo;
+		for step in 0..40 {
+			board = gol(&board);
+			let view = render(&board);
+			print!("{}[2J", 27 as char);
+			print!("{}",&view);
+			thread::sleep(millis);
+		}
+    }
 }
